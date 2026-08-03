b37f05f0d6c78bc28280d98b0c2b42983c04d93b
DarkflameMG <deknan70@gmail.com>
2026-08-03 00:46:13 -0400

    search book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
   const author = req.params.author;
   const book = Object.values(books).filter(book => book.author === author);
   res.send(JSON.stringify(book,null,3))
@@ -34,8 +33,10 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const book = Object.values(books).filter(book => book.title.includes(title));
+  res.send(JSON.stringify(book,null,3))
+  return res.status(200).json({message: "Get book by tital OK"});
 });
 
 //  Get book review

df63cf635e0b14dc8cc6aec52fe198f81bbbe69c
DarkflameMG <deknan70@gmail.com>
2026-08-03 05:05:28 -0400

    add promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,6 +30,7 @@ function getAllBooks(){
         }
     });
 }
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
 //   res.send(JSON.stringify(books,null,3));
@@ -41,35 +42,102 @@ public_users.get('/',function (req, res) {
     }));
 });
 
+function getBooksByISBN(isbn){
+    return new Promise((resolve, reject) => {
+        const result = Object.values(books[isbn]);
+        if(result.length > 0){
+            resolve(result);
+        } else {
+            reject("No books found");
+        }
+    });
+}
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(JSON.stringify(books[isbn],null,3));
-  return res.status(200).json({message: "Get book by ISBN OK"});
- });
+//   const isbn = req.params.isbn;
+//   res.send(JSON.stringify(books[isbn],null,3));
+//   return res.status(200).json({message: "Get book by ISBN OK"});
+    getBooksByISBN(req.params.isbn)
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json({
+        message: err
+    }))
+});
+
+function getBooksByAuthor(author){
+    return new Promise((resolve, reject) => {
+        const result = Object.values(books).filter(book => {
+            return book.author.includes(author);
+        });
+        if(result.length > 0){
+            resolve(result);
+        } else {
+            reject("No books found");
+        }
+    });
+}
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  const book = Object.values(books).filter(book => {
-    return book.author === author;
-  });
-  res.send(JSON.stringify(book,null,3))
-  return res.status(200).json({message: "Get book by author OK"});
+//   const author = req.params.author;
+//   const book = Object.values(books).filter(book => {
+//     return book.author === author;
+//   });
+//   res.send(JSON.stringify(book,null,3))
+//   return res.status(200).json({message: "Get book by author OK"});
+    getBooksByAuthor(req.params.author)
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json({
+        message: err
+    }))
 });
+
+function getBooksByTitle(title){
+    return new Promise((resolve, reject) => {
+        const result = Object.values(books).filter(book => {
+            return book.title.includes(title);
+        });
+        if(result.length > 0){
+            resolve(result);
+        } else {
+            reject("No books found");
+        }
+    });
+}
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  const book = Object.values(books).filter(book => book.title.includes(title));
-  res.send(JSON.stringify(book,null,3))
-  return res.status(200).json({message: "Get book by tital OK"});
+//   const title = req.params.title;
+//   const book = Object.values(books).filter(book => book.title.includes(title));
+//   res.send(JSON.stringify(book,null,3))
+//   return res.status(200).json({message: "Get book by tital OK"});
+    getBooksByTitle(req.params.title)
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json({
+        message: err
+    }))
 });
+
+function getBooksReview(isbn){
+    return new Promise((resolve, reject) => {
+        const result = books[isbn].reviews
+        if(result){
+            resolve(result);
+        } else {
+            reject("No review found");
+        }
+    });
+}
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  getBooksReview(req.params.isbn)
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json({
+        message: err
+    }))
 });
 
 module.exports.general = public_users;
